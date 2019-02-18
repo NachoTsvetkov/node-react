dd8c8807df848bbee0856f3c7f3df15ed3988418
NTsvetkov <NTsvetkov@prevalent.net>
2019-02-18 15:11:33 +0200

    modules root cleanup

--- clean/modules/my-first.js
+++ clean/modules/my-first.js
@@ -2,6 +2,7 @@ var http = require('http');
 var dt = require('./my-first-module');
 
 http.createServer(function (req, res) {
-  dt.myDateTime();
+  res.write("Date: " + dt.myDateTime());
+  res.end();
 }).listen(8080);
 
