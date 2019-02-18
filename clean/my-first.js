14bd24456287a03a5fab7f5863b46c6e4ef86b76
NTsvetkov <NTsvetkov@prevalent.net>
2019-02-18 14:54:57 +0200

    demo read file created

--- clean/my-first.js
+++ clean/my-first.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var dt = require('./my-first-module');
+var url = require('url');
 var fs = require('fs');
+
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
 }).listen(8080);
