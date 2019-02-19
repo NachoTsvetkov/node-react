9167ae98c7a68afd0fbf1844e6244874bbe7a9fa
NachoTsvetkov <nacho.tsvetkov@gmail.com>
2019-02-19 18:49:14 +0200

    typescript first build

---
+++ typescript/greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello " + person + "!";
+}
+var nameToGreet = 'Node Hero';
+console.log(greeter(nameToGreet));
---
+++ typescript/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return `Hello ${person}!`
+}
+  
+const nameToGreet = 'Node Hero'
+
+console.log(greeter(nameToGreet))
