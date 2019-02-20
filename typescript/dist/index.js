a8a897c997df36615834c851201818a7c98f0c96
NachoTsvetkov <nacho.tsvetkov@gmail.com>
2019-02-20 12:01:45 +0200

    npm scripts fixed

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const App_1 = require("./App");
-const port = App_1.default.env.PORT || 3000;
+const port = 3000;
 App_1.default.listen(port, (err) => {
     if (err) {
         return console.log(err);
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,6 @@
 import app from './App';
 
-const port = app.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, (err: any) => {
   if (err) {
